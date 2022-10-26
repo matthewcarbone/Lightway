e68ce303d0d87d4acdcccf04c48d5923c0db1c41
Charles Clark <Charles.Clark@stonybrook.edu>
2022-10-26 11:16:17 -0400

    docstrings

--- aimmdb/_tests/test_eli.py
+++ aimmdb/_tests/test_eli.py
@@ -1,10 +1,10 @@
-from aimmdb.ingest.eli import ingest
-from pathlib import Path
+from aimmdb.ingest.eli import ingest, read
 
 
 def test_ingest():
-    test_path = Path("./eli_test_file.dat")
-    data, md = ingest(test_path)
+    test_path = "eli_test_file.dat"
+    data, md, uid = ingest(test_path, return_uid=True)
 
+    print(uid)
     print(data)
     print(md)
--- aimmdb/ingest/eli.py
+++ aimmdb/ingest/eli.py
@@ -3,41 +3,65 @@ import pandas as pd
 from pathlib import Path
 
 
-def read_metadata_and_header(path: Path):
-    metadata = {}
-    header = ""
+def read_metadata_and_header(path):
+    """Read through commented lines of dat file to get metadata and DataFrame header
+
+    Parameters
+    ----------
+    path: str, or path object
+        path to .dat file from ISS beamline
+
+    Returns
+    -------
+    tuple[dict[str, str], str]
+        First element is dictionary of metadata.
+        Second element is header as single string, with column names separated by whitespace.
+    """
+    metadata: dict
+    header: str
     with open(path, "r") as dat_file:
         lines = dat_file.readlines()
+        # remove starting hash and whitespace
+        comment_lines = [line[2:] for line in lines if line.startswith("#")]
+
         metadata = {
-            # remove starting hash and whitespace, then split at colon
-            line[2:].split(":", maxsplit=1)[0]: 
-            line[2:].split(":", maxsplit=1)[1].strip()
-            for line in lines
-            if ":" in line and line.startswith("#")
+            # split line at colon for key-value pairs and strip whitespace from values
+            line.split(":", maxsplit=1)[0]: line.split(":", maxsplit=1)[1].strip()
+            for line in comment_lines
+            if ":" in line
         }
 
-        # # remove starting hash and whitespace
-        # lines = [line[2:] for line in lines if line.startswith("#")]
-        # md_kv = [line.split(":", maxsplit=1) for line in lines if ":" in line]
-        # metadata = {key: value.strip() for key, value in md_kv}
+        # only comment line without colon should be header
+        header = [line for line in comment_lines if ":" not in line]
+        header = header[0]
 
-        for line in lines:
-            if ":" in line:
-                # key, value = line.split(":", maxsplit=1)
-                # metadata[key] = value.strip()
-                pass
-            else:
-                header = line
     return metadata, header
 
 
-def ingest(path: Path, return_uid=False):
+def ingest(path, return_uid=False):
+    """Prepare scan data from Eli (ISS beamline) for entry into AIMMDB
+
+    Parameters
+    ----------
+    path: str, or path object
+        path to .dat file from ISS beamline
+
+    Returns
+    -------
+    tuple[DataFrame, dict[str, str]]
+        First element is DataFrame of column data from file.
+        Second element is the metadata in a dictionary of str key-value pairs.
+
+    Other Parameters
+    ----------------
+    return_uid: bool
+        Optional keyword to separately return uid of scan. 
+        The uid should be present in the metadata, but it is an especially important 
+        piece of information that may be useful to store separately.
+    """
     md, hdr = read_metadata_and_header(path)
 
-    hdr = "energy  i0  it  ir  iff  aux1  aux2  aux3  aux4"
-
     temp_data = pd.read_csv(path, sep="\s+", comment="#", names=hdr.split())
-    # temp_data = pd.read_csv(path, sep="\s+", comment="#", header=-1)
     temp_data["mu_trans"] = -np.log(temp_data["it"] / temp_data["i0"])
     temp_data["mu_fluor"] = -(temp_data["iff"] / temp_data["i0"])
     temp_data["mu_ref"] = -np.log(temp_data["ir"] / temp_data["i0"])
